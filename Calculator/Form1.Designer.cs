076d696aac6ba62735ac0b2d50fa270440409ad1
thuhuong <ltthuong2132002@gmail.com>
2022-10-31 12:36:31 +0700

    Update form1

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -32,13 +32,15 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtSoa = new System.Windows.Forms.TextBox();
             this.txtSob = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(137, 69);
+            this.label1.Location = new System.Drawing.Point(135, 141);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(68, 35);
             this.label1.TabIndex = 0;
@@ -48,7 +50,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(137, 142);
+            this.label2.Location = new System.Drawing.Point(135, 204);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(70, 35);
             this.label2.TabIndex = 0;
@@ -57,7 +59,7 @@
             // txtSoa
             // 
             this.txtSoa.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtSoa.Location = new System.Drawing.Point(263, 71);
+            this.txtSoa.Location = new System.Drawing.Point(263, 143);
             this.txtSoa.Name = "txtSoa";
             this.txtSoa.Size = new System.Drawing.Size(273, 34);
             this.txtSoa.TabIndex = 1;
@@ -65,16 +67,40 @@
             // txtSob
             // 
             this.txtSob.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtSob.Location = new System.Drawing.Point(263, 142);
+            this.txtSob.Location = new System.Drawing.Point(263, 204);
             this.txtSob.Name = "txtSob";
             this.txtSob.Size = new System.Drawing.Size(273, 34);
             this.txtSob.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(521, 311);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 54);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Cộng";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(317, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(199, 46);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "PHÉP TÍNH";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.txtSob);
             this.Controls.Add(this.txtSoa);
             this.Controls.Add(this.label2);
@@ -92,5 +118,7 @@
         private Label label2;
         private TextBox txtSoa;
         private TextBox txtSob;
+        private Button button1;
+        private Label label3;
     }
 }
